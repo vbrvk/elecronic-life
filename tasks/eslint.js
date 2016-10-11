501861b560184da12e340799a02e3328876130aa
Vladimir Borovik <bobahbdb@gmail.com>
2016-10-11 18:26:07 +0300

    fix bug with start eslint without tmp directory

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -14,6 +14,7 @@ module.exports = function (opt) {
 		try {
 			eslintCashe = JSON.parse(fs.readFileSync(eslintFileCashe));
 		} catch (e) {
+			fs.mkdirSync(process.cwd() + '/tmp/');
 			console.log('Cash is empty');
 		}
 

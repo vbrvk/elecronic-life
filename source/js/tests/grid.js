6dbaaf88a3ad5e34e7c573af8adadbfcd74ba9ee
Vladimir Borovik <bobahbdb@gmail.com>
2016-10-12 21:25:01 +0300

    Add Grid tests

---
+++ source/js/tests/grid.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+
+import Grid from '../grid.js';
+
+chai.should();
+
+describe('Grid of world', () => {
+	describe('Props of grid', () => {
+		it('Should return new grid with height 20', () => {
+			(new Grid(20, 30)).height.should.to.eql(20);
+		});
+		it('Should return new grid with width 20', () => {
+			(new Grid(20, 30)).width.should.to.eql(30);
+		});
+		it('Should return new grid with lenght 20*30', () => {
+			(new Grid(20, 30)).space.should.to.have.lengthOf(30 * 20);
+		});
+	});
+});

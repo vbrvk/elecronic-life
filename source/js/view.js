d75402b4e9d81e5f29c4d5d945a0a76d1327252a
Vladimir Borovik <bobahbdb@gmail.com>
2016-10-14 20:15:48 +0300

    Add to View methods find and findAll

--- source/js/view.js
+++ source/js/view.js
@@ -1,4 +1,6 @@
+import { random, isEmpty } from 'underscore';
 import { directions } from './world-constants';
+import { World } from './world';
 
 export default class View {
 	constructor(world, vector) {
@@ -8,8 +10,23 @@ export default class View {
 	look(direction) {
 		const target = this.vector.plus(directions[direction]);
 		if (this.world.grid.isInside(target)) {
-			return this.world.grid.get(target);
+			return World.charFromElement(this.world.grid.get(target));
 		}
 		return '#';
+	}
+	findAll(char) {
+		const found = [];
+		Object.keys(directions).forEach((dir) => {
+			if (this.look(dir) === char) {
+				found.push(dir);
+			}
+		});
+		return found;
+	}
+
+	find(char) {
+		const found = this.findAll(char);
+		if (isEmpty(found)) return null;
+		return random(0, found.length - 1);
 	}
 }

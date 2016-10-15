400f71b87924cc34bdb9babaf276f7401db859a3
Vladimir Borovik <bobahbdb@gmail.com>
2016-10-15 20:49:33 +0300

    Add actionTypes

--- source/js/world-constants.js
+++ source/js/world-constants.js
@@ -1,4 +1,5 @@
 import Vector from './vector.js';
+import { World } from './world';
 
 const directions = {
 	n: new Vector(0, -1),
@@ -24,5 +25,64 @@ const plan = ['############################',
             '#    #                     #',
             '############################'];
 
-export default { directions, plan };
-export { directions, plan };
+/* eslint no-param-reassign: ["error", { "props": false }]*/
+const actionTypes = {
+	default: (opt) => {
+		opt.critter.energy -= 0.2;
+		if (opt.critter.energy <= 0) {
+			opt.world.grid.set(opt.vector, null);
+		}
+	},
+	grow: (opt) => {
+		opt.critter.energy += 0.5;
+		return true;
+	},
+	move: (opt) => {
+		const world = opt.world;
+		const critter = opt.critter;
+		const vector = opt.vector;
+		const action = opt.action;
+
+		const destination = world.checkDestination(action, vector);
+		if (destination == null || critter.energy <= 1 || world.grid.get(vector) !== ' ') {
+			return false;
+		}
+		critter.energy -= 1;
+		world.grid.set(vector, null);
+		world.grid.set(destination, critter);
+		return true;
+	},
+	eat: (opt) => {
+		const world = opt.world;
+		const critter = opt.critter;
+		const vector = opt.vector;
+		const action = opt.action;
+
+		const destination = world.checkDestination(action, vector);
+		const crittDest = destination != null && world.grid.get(destination);
+		if (!crittDest || crittDest.energy == null) {
+			return false;
+		}
+		critter.energy += crittDest.energy;
+		world.grid.set(destination, null);
+		return true;
+	},
+	reproduce: (opt) => {
+		const world = opt.world;
+		const critter = opt.critter;
+		const vector = opt.vector;
+		const action = opt.action;
+
+		const baby = World.elementFromChar(world, critter.originChar);
+		const dest = world.checkDestination(action, vector);
+		if (dest == null || critter.energy <= 2 * baby.energy || world.grid.get.dest != null) {
+			return false;
+		}
+		world.grid.set(dest, baby);
+		critter.energy -= baby.energy * 2;
+		return true;
+	},
+};
+
+export default { directions, plan, actionTypes };
+export { directions, plan, actionTypes };

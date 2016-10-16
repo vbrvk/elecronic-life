c284c23f4564c9916ac9e9a1a60e03c03d3be974
Vladimir Borovik <bobahbdb@gmail.com>
2016-10-16 18:11:16 +0300

    Add new world with new critters

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,30 @@
-import { plan } from './world-constants';
-import { World, Wall } from './world';
-import BouncingCritter from './critters/bouncing-criters';
+// import { plan } from './world-constants';
+import { LifeLikeWorld, Wall } from './world';
+// import BouncingCritter from './critters/bouncing-criters';
+import Plant from './critters/plant';
+import PlantEater from './critters/plant-eater';
 
-const world = new World(plan, {
-	'#': Wall,
-	o: BouncingCritter,
-});
+const world = new LifeLikeWorld(
+	['############################',
+   '#####                 ######',
+   '##   ***                **##',
+   '#   *##**         **  O  *##',
+   '#    ***     O    ##**    *#',
+   '#       O         ##***    #',
+   '#                 ##**     #',
+   '#   O       #*             #',
+   '#*          #**       O    #',
+   '#***        ##**    O    **#',
+   '##****     ###***       *###',
+   '############################'],
+  { '#': Wall,
+   O: PlantEater,
+   '*': Plant }
+);
 
 const container = document.querySelector('.container');
 const text = container.appendChild(document.createTextNode(world.toString()));
 setInterval(() => {
 	world.turn();
 	text.textContent = world.toString();
-}, 700);
+}, 300);
